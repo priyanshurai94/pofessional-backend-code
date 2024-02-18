1b266750b096a167d9d025bda2f5ceb0ede8c1e5
prianshu143 <110248829+prianshu143@users.noreply.github.com>
2024-02-18 16:43:37 +0530

    Initial commit

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,5 +1,10 @@
-const asyncHandler = (requesthandler) => {
+const asyncHandler = (requestHandler) => {
      return (req, res, next) => {
-        Promise.resolve(requesthandler(req, res, next)).catch((err) => next(err));
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
     };
 };
+
+
+
+
+export {asyncHandler}
