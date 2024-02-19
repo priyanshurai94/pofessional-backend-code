e6a44de5a02d591c3bb2f6dfa4bedb0cbec3ae67
prianshu143 <110248829+prianshu143@users.noreply.github.com>
2024-02-19 16:39:42 +0530

    intialized

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -14,3 +14,5 @@ class ApiError extends Error {
     this.errorrs = errors
 }
 }
+
+export {ApiError}
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -2,46 +2,30 @@ import {v2 as cloudinary} from "cloudinary"
 import fs from "fs"
 
 
-          
 cloudinary.config({ 
-  cloud_name: CLOUDINARY_CLOUD_NAME, 
-  api_key: CLOUDINARY_API_KEY, 
-  api_secret: CLOUDINARY_API_SECRET
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET 
 });
 
-
-const uploadOnCloudinary = async (localFilepath) =>{
+const uploadOnCloudinary = async (localFilePath) => {
     try {
-        if(!localFilepath) return null
+        if (!localFilePath) return null
         //upload the file on cloudinary
-
-        const response = await cloudinary.uploader.upload
-        (localFilepath,{
+        const response = await cloudinary.uploader.upload(localFilePath, {
             resource_type: "auto"
         })
-        // file has been upload succesfully
-        console.log("file is uploaded on cloudinary", response.url);
-
+        // file has been uploaded successfull
+        //console.log("file is uploaded on cloudinary ", response.url);
+        fs.unlinkSync(localFilePath)
         return response;
-        
-    } catch (error) {
-        fs.unlinkSync(localFilepath) // remove the temporarly saved file if file upload operation is failed
-        return null;
-        
-    }
 
+    } catch (error) {
+        fs.unlinkSync(localFilePath) // remove the locally saved temporary file as the upload operation got failed
+        return null;
+    }
 }
 
+
+
 export {uploadOnCloudinary}
-
-
-
-
-
-
-
-
-
-cloudinary.uploader.upload("https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
-  { public_id: "olympic_flag" }, 
-  function(error, result) {console.log(result); });
